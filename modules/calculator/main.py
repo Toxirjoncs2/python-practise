8ca133031436d58261b2017adf3f1c1ff844e1a8
Efrain Battleship <efrainba@te-e10.tash.21-school.uz>
2026-07-13 17:55:31 +0500

    calculator

---
+++ modules/calculator/main.py
@@ -0,0 +1,7 @@
+import operations
+x=int(input("Enter the 1st number: "))
+y=int(input("Enter the 2nd number: "))
+print(operations.add(x,y))
+print(operations.subtract(x,y))
+print(operations.multiply(x,y))
+print(operations.divide(x,y))
